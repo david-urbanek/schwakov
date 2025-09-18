c9f0ed1b819e3e30c0966cf484cb279bd964ba9e
David Urbanek <urbanek.dav@email.cz>
2025-09-18 10:21:30 +0200

    fix(config): correct GTM config

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import {Contact7} from "@/app/components/Contact";
 import Header from "@/app/components/Header";
 import CookieBanner from "@/app/components/CookieBanner";
 import Script from 'next/script'
+import {GoogleTagManager} from "@next/third-parties/google";
 
 const inter = Inter({
     subsets: ["latin", "latin-ext" ],
@@ -35,22 +36,9 @@ export default function RootLayout({
   return (
 
     <html lang="cs" className="scroll-smooth">
-    <head>
-        <Script id="google-tag-manager" strategy="afterInteractive">
-            {`function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-MFPRXK8S';`}
-        </Script>
-    </head>
+    <GoogleTagManager gtmId={"GTM-MFPRXK8S"}></GoogleTagManager>
       <body
           className={`${inter.variable} antialiased flex flex-col justify-center px-4 md:px-8 py-6 md:py-8 gap-8 min-h-screen bg-background text-foreground`}>
-        <noscript>
-            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MFPRXK8S"
-                            height="0" width="0" style={{display: 'none', visibility: 'hidden'}}>
-
-            </iframe>
-        </noscript>
       <Header></Header>
       <main className="flex flex-col gap-20 md:gap-28 max-w-7xl mx-auto w-full">
           {children}
